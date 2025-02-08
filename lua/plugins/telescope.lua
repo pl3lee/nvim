9a7dc39ec52494f1ab01b0a8b45e49813a7dd1e8
pl3lee <billy.pl.lee@gmail.com>
2025-02-08 17:29:53 -0500

    hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,13 @@ return {
             'nvim-lua/plenary.nvim'
         },
         config = function()
+            require('telescope').setup({
+                defaults = {
+                    hidden = true,  -- Show hidden files (dotfiles)
+                    file_ignore_patterns = { ".git" }
+                }
+            })
+
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
             vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
